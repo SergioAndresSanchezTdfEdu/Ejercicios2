9f501d9a4d9920c41fde666b2c9a66793de7cc53
Sergio Andrés SÁNCHEZ <103612998+SergioAndresSanchezTdfEdu@users.noreply.github.com>
2022-05-29 18:04:22 -0300

    Todo al 29/02/2022

--- Ejercicios/Clase9-Consulta-19-05-22/SumarDosArreglos/sumardosarreglos.ts
+++ Ejercicios/Clase9-Consulta-19-05-22/SumarDosArreglos/sumardosarreglos.ts
@@ -1,69 +1,77 @@
 document.getElementById("app").innerHTML = `
   <h1>Sumar Dos Arreglos</h1>
 `;
+// JOSE: llamar a las variables y funciones con minusculas como primer palabra
+// SERGIO: ok.
+let arreglo1: number[] = new Array(6);
+let arreglo2: number[] = new Array(6);
+let sumaArreglo1y2: number[] = new Array(6);
 
-let Arreglo1: number[] = new Array(6);
-let Arreglo2: number[] = new Array(6);
-let SumaArreglo1y2: number[] = new Array(6);
+// JOSE: seria mejor llamar a esta funcion cargarArreglo y no LeerArreglo
+// SERGIO: Perfecto!!!
 
-function LeerArreglo(
-  Arreglo: number[],
-  Tamanio: number,
-  NombreArreglo: string
-): number[] {
-  let NumeroLeido: number;
-  let CopiaArreglo: number[] = new Array(Tamanio); //Copio para no modificar el que ingresa.
-  for (let i: number = 0; i < Tamanio; i++) {
-    NumeroLeido = Number(
+function cargarArreglo(
+  arreglo: number[],
+  tamanio: number,
+  nombreArreglo: string
+) {
+  // JOSE: definir variables fuera de ciclos
+  // SERGIO: perdón, jajaja.
+
+  let numeroLeido: number;
+  for (let i: number = 0; i < tamanio; i++) {
+    numeroLeido = Number(
       prompt(
-        `Indique el número que desea incorporar en la posición ${i} del ${NombreArreglo}: `
+        `Indique el número que desea incorporar en la posición ${i} del ${nombreArreglo}: `
       )
     );
-    CopiaArreglo[i] = NumeroLeido;
+    arreglo[i] = numeroLeido;
   }
-  return CopiaArreglo;
 }
 
-function SumarArreglo(
+// JOSE: los arreglos son globales no hace falta retornarlos
+// SERGIO: buenísmo, no lo sabía.
+
+function sumarArreglos(
   arreglo1: number[],
   arreglo2: number[],
   tamanio: number
-): number[] {
-  let SumaArreglos: number[] = new Array(tamanio);
+) {
   for (let i: number = 0; i < tamanio; i++) {
-    SumaArreglos[i] = arreglo1[i] + arreglo2[i];
+    sumaArreglo1y2[i] = arreglo1[i] + arreglo2[i];
+    // JOSE: porque no usar el arreglo de suma ya definido al principio del script?
+    // SERGIO: porque no sabía que los arreglos eran Globales y se modificaban
+    //         dentro de una función.
   }
-  return SumaArreglos;
 }
 
-function MostrarArreglo(
+function mostrarArreglo(
   arreglo: number[],
   tamanio: number,
-  NombreArreglo: string
+  nombreArreglo: string
 ) {
   for (let i: number = 0; i < tamanio; i++) {
     console.log(
-      `El dato en la posición ${i} del ${NombreArreglo} es ${arreglo[i]}`
+      `El dato en la posición ${i} del ${nombreArreglo} es ${arreglo[i]}`
     );
   }
 }
 
-Arreglo1 = LeerArreglo(Arreglo1, 6, "Arreglo 1");
-MostrarArreglo(Arreglo1, 6, "Arreglo 1");
-Arreglo2 = LeerArreglo(Arreglo2, 6, "Arreglo 2");
-MostrarArreglo(Arreglo2, 6, "Arreglo 2");
-SumaArreglo1y2 = SumarArreglo(Arreglo1, Arreglo2, 6);
-MostrarArreglo(SumaArreglo1y2, 6, "arreglo de la suma de los Arreglos 1 y 2");
+cargarArreglo(arreglo1, 6, "Arreglo 1");
+// SERGIO: Cambiado por comentario de JOSE
+//mostrarArreglo(arreglo1, 6, "Arreglo 1");
+console.log(`El Arreglo 1 es: ${arreglo1}`);
+cargarArreglo(arreglo2, 6, "Arreglo 2");
+// SERGIO: Cambiado por comentario de JOSE
+//mostrarArreglo(arreglo2, 6, "Arreglo 2");
+console.log(`El Arreglo 2 es: ${arreglo2}`);
+sumarArreglos(arreglo1, arreglo2, 6);
+// SERGIO: Cambiado por comentario de JOSE
+//mostrarArreglo(sumaArreglo1y2, 6, "arreglo de la suma de los Arreglos 1 y 2");
+console.log(`La Suma de los Arreglos 1 y 2 es: ${sumaArreglo1y2}`);
 
-/* Podría llamar todo así y no necesitaría definir Arreglo1, Arreglo2 y SumaArreglo1y2
-MostrarArreglo(
-  SumarArreglo(
-    LeerArreglo(Arreglo1, 6, "Arreglo 1"),
-    LeerArreglo(Arreglo2, 6, "Arreglo 2"),
-    6
-  ),
-  6,
-  "arreglo de la suma de los Arreglos 1 y 2"
-);
-*/
-// test pull
+// JOSE: a veces no es bueno mostrar tanta informacion, console.log arreglo es suficiente y claro
+// SERGIO: Debería reemplasar los mostrarArreglo(...) por console.log y listo
+
+// JOSE: El ejercicio esta bien resuelto
+// SERGIO: Gracias!!!
