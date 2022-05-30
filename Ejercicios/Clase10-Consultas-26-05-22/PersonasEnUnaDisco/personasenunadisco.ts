51a7aebe83b93ca9782613df8df850b7f0781e0c
Sergio Andrés SÁNCHEZ <103612998+SergioAndresSanchezTdfEdu@users.noreply.github.com>
2022-05-30 01:25:52 -0300

    Todo al 29/05/2022 con correcciones de Jose

--- Ejercicios/Clase10-Consultas-26-05-22/PersonasEnUnaDisco/personasenunadisco.ts
+++ Ejercicios/Clase10-Consultas-26-05-22/PersonasEnUnaDisco/personasenunadisco.ts
@@ -10,8 +10,13 @@ let mayoresMenores: number[] = new Array(2);
 // Retorna un entero aleatorio entre min (incluido) y max (excluido),
 // Como el max se excluía, se agrego el + 1.
 // ¡Usando Math.round() te dará una distribución no-uniforme!
-function numAlAzar(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+//function numAlAzar(min: number, max: number) {
+//  return Math.floor(Math.random() * (max - min + 1)) + min;
+//}
+
+// JOSE: Función de él.
+function numAlAzar(min: number, max: number) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 // Carga un arreglo en todo su longitud con edades al azar de personas
@@ -26,6 +31,9 @@ function cargarArregloNumeroAzar(arreglo: number[]) {
 // puntoDeCorte dado en mayores e iguales y menores al puntoDeCorte y
 // las almacena e un arreglo de 2 posisicones, siendo la posición 0 la
 // cantidad de mayores e iguales y la posisción 1 la cantidad de menores.
+// JOSE: poniendonos finos en la correccion me hace un poco de ruido el
+//       nombre de la funcion, debe ser descriptivo y una accion,
+//       por ej: cargaArrContador o algo incluso mejor que eso jaja
 function mayoresA(arreglo: number[], puntoDeCorte: number) {
   mayoresMenores[0] = 0;
   mayoresMenores[1] = 0;
@@ -49,3 +57,5 @@ console.log(
 console.log(
   `La Cantidad Total de Personas en la Disco es de: ${totalDePersonas}`
 );
+console.log("Arerglo: " + edadDePersonas);
+// JOSE: muy bien tu solucion!
